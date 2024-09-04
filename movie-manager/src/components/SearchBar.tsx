9b19ef81ac90859ff77b5d1a84e784a128a5ae10
Todor Krumov <baito6u@gmail.com>
2024-09-04 07:00:04 +0300

    Create SearchBar.tsx

---
+++ movie-manager/src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+// src/components/SearchBar.tsx
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchQuery } from '../store/moviesSlice';
+import { RootState } from '../store/store';
+import './SearchBar.css'; // Optional: For styling
+
+const SearchBar: React.FC = () => {
+  const dispatch = useDispatch();
+  const searchQuery = useSelector((state: RootState) => state.movies.searchQuery);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchQuery(e.target.value));
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search movies by title..."
+        value={searchQuery}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
