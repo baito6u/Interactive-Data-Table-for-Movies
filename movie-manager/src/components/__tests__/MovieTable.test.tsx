046b9b46a0878221a5f6ca9b826eae5670a90acc
Todor Krumov <baito6u@gmail.com>
2024-09-05 07:18:24 +0300

    Create MovieTable.test.tsx

---
+++ movie-manager/src/components/__tests__/MovieTable.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../store/store';
+import MovieTable from '../MovieTable';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('MovieTable Component', () => {
+  test('renders movie table with movies', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <MovieTable />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const movieTitle = screen.getByText(/Following/i);
+    expect(movieTitle).toBeInTheDocument();
+  });
+
+  test('search functionality works', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <MovieTable />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const searchInput = screen.getByPlaceholderText(/Search movies by title.../i);
+    expect(searchInput).toBeInTheDocument();  // This will also work
+  });
+});
